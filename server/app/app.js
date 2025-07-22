890a907ba1184ea52eac5ba725d6f1d4e9703ebe
ArdanyNJ <danyrazaq8@gmail.com>
2025-07-22 20:02:42 +0700

    tes1

--- server/app/app.js
+++ server/app/app.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 
 const authRoutes = require('./routes/auth.routes');
 const wasteRoutes = require('./routes/waste.routes');
+const bankRoutes = require('./routes/bank.routes');
 
 const app = express();
 
@@ -13,8 +14,8 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/waste', wasteRoutes);
+app.use('/api/bank', bankRoutes);
 
 module.exports = app;
